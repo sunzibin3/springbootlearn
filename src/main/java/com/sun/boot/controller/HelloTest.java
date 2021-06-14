8b924be092ae99a83df83bdc3f6a2b92891e0ff2
sunzibin <sunzibin@sunline.cn>
2021-06-14 14:51:13 +0800

    second_commit

--- src/main/java/com/sun/boot/controller/HelloTest.java
+++ src/main/java/com/sun/boot/controller/HelloTest.java
@@ -1,13 +1,14 @@
 package com.sun.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloTest {
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public String hello(@RequestParam("name") String name){
+        return "Hello World"+name;
     }
 }
